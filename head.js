e74af339c107fb6b49f89a6befc4a6d436268e3a
Tom <chiatunglo@hotmail.com>
2022-05-20 04:33:30 +0000

    Module 1 Week 1 Implement head

---
+++ head.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return `✅️✅️✅️Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    return `🔴️🔴️🔴️Assertion Failed: ${actual} !== ${expected}`;
+  }
+
+};
+
+const head = function(arr) {
+  return arr[0]
+}
+
+console.log(assertEqual(head([5,6,7]), 5));
+console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
