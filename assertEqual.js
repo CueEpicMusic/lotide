7d454c9c8fab79c6296eb5826d61b7c5a8c26d30
Tom <chiatunglo@hotmail.com>
2022-05-20 03:48:55 +0000

    Module 1 Week 1 assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return `✅️✅️✅️Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    return `🔴️🔴️🔴️Assertion Failed: ${actual} !== ${expected}`;
+  }
+
+};
+
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1, 1));
