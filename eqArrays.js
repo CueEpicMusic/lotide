cc68cb7d6afc9ff62ae4c8dc249ae333067470ac
Tom <chiatunglo@hotmail.com>
2022-05-21 15:59:32 +0000

    Module 1 Week 1 Implement eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return `✅️✅️✅️Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    return `🔴️🔴️🔴️Assertion Failed: ${actual} !== ${expected}`;
+  }
+
+};
+
+const eqArrays = function(a, b) {
+  if (a.length != b.length) {
+    return false;
+  } else {
+    for (i in a) {
+      if (a[i] !== b[i]) {
+        return false
+      }
+    }
+    return true
+  }
+}
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)); // => should PASS
