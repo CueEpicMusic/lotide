346d46a17cbfd5bba5d1384a26a6ace44cde99c3
Tom <chiatunglo@hotmail.com>
2022-05-23 21:17:38 +0000

    Module 1 Week 1 Implement middle

---
+++ middle.js
@@ -0,0 +1,38 @@
+// const eqArrays = function(a, b) {
+//   if (a.length != b.length) {
+//     return false;
+//   } else {
+//     for (i in a) {
+//       if (a[i] !== b[i]) {
+//         return false
+//       }
+//     }
+//     return true
+//   }
+// };
+
+// const assertEqual = function(actual, expected) {
+//   if (eqArrays(actual, expected)) {
+//     return `✅️✅️✅️Assertion Passed: ${actual} === ${expected}`;
+//   } else {
+//     return `🔴️🔴️🔴️Assertion Failed: ${actual} !== ${expected}`;
+//   }
+
+// };
+
+const middle = function(array) {
+  if (array.length <= 2) {
+    return [];
+  } else if (array.length % 2 === 0) {
+    return [array[Math.ceil(array.length / 2) - 1], array[Math.ceil(array.length / 2)]];
+  } else {
+    return [array[Math.floor(array.length / 2)]]
+  }
+};
+
+console.log(middle([1])) // => []
+console.log(middle([1, 2])) // => []
+console.log(middle([1, 2, 3])) // => [2]
+console.log(middle([1, 2, 3, 4, 5])) // => [3]
+console.log(middle([1, 2, 3, 4])) // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
