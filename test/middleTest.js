ae3a8405370ab64e60c718e966370c6bc9689663
Tom <chiatunglo@hotmail.com>
2022-06-09 03:52:41 +0000

    refactored test files with mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,28 @@
 const assertArraysEqual = require(`../assertArraysEqual`)
 const middle = require(`../middle`)
+const assert = require(`chai`).assert;
 
-console.log(assertArraysEqual(middle([1]), [])) // => []
-console.log(assertArraysEqual(middle([1, 2]), [])) // => []
-console.log(assertArraysEqual(middle([1, 2, 3]), [2])) // => [2]
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]),[3])) // => [3]
-console.log(assertArraysEqual(middle([1, 2, 3, 4]), [2, 3])) // => [2, 3]
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])) // => [3, 4]
+// console.log(assertArraysEqual(middle([1]), [])) // => []
+// console.log(assertArraysEqual(middle([1, 2]), [])) // => []
+// console.log(assertArraysEqual(middle([1, 2, 3]), [2])) // => [2]
+// console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]),[3])) // => [3]
+// console.log(assertArraysEqual(middle([1, 2, 3, 4]), [2, 3])) // => [2, 3]
+// console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])) // => [3, 4]
+
+describe(`#middle`, () => {
+  it(`returns [1] for []`, () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it(`returns [1, 2] for []`, () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+  it(`returns [1, 2, 3] for [2]`, () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it(`returns [1, 2, 3, 4] for [2, 3]`, () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it(`returns [1, 2, 3, 4, 5, 6] for [3, 4]`, () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,16 @@
-const tail = require(`../tail`)
+const tail = require(`../tail`);
+const assert = require('chai').assert;
 const assertEqual = require(`../assertEqual`)
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-console.log(assertEqual(result, ["Lighthouse", "Labs"]));
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// console.log(assertEqual(result, ["Lighthouse", "Labs"]));
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-console.log(assertEqual(words.length, 3));
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words);
+// console.log(assertEqual(words.length, 3));
+
+describe(`#tail`, () => {
+  it(`returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']`, () => {
+    assert.strictEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
+});
