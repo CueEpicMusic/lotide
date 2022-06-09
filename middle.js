524373552bea537efeab6d59ecc61833a4622132
Tom <chiatunglo@hotmail.com>
2022-06-09 02:51:32 +0000

    added middleTest

--- middle.js
+++ middle.js
@@ -1,23 +1,4 @@
-const eqArrays = function(a, b) {
-  if (a.length != b.length) {
-    return false;
-  } else {
-    for (i in a) {
-      if (a[i] !== b[i]) {
-        return false
-      }
-    }
-    return true
-  }
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    return `✅️✅️✅️Assertion Passed: ${actual} === ${expected}`;
-  } else {
-    return `🔴️🔴️🔴️Assertion Failed: ${actual} !== ${expected}`;
-  }
-};
+const assertArraysEqual = require(`./assertArraysEqual`)
 
 const middle = function(array) {
   if (array.length <= 2) {
@@ -29,9 +10,4 @@ const middle = function(array) {
   }
 };
 
-console.log(assertArraysEqual(middle([1]), [])) // => []
-console.log(assertArraysEqual(middle([1, 2]), [])) // => []
-console.log(assertArraysEqual(middle([1, 2, 3]), [2])) // => [2]
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]),[3])) // => [3]
-console.log(assertArraysEqual(middle([1, 2, 3, 4]), [2, 3])) // => [2, 3]
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])) // => [3, 4]
+module.exports = middle;
